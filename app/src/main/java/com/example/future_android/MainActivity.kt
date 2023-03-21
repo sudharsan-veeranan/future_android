f406c4e8e65637f0be9891eb2384e3fdce5ee237
sudharsan-v <sudharsan.v@stellantis.com>
2023-03-21 23:02:19 +0530

    API introduction - Make network call using retrofit

--- app/src/main/java/com/example/future_android/MainActivity.kt
+++ app/src/main/java/com/example/future_android/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.future_android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
+var TAG = "Retro"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var nwCall = Retrofit.myRetrofit.getUsers()
+        nwCall.enqueue( object : Callback<List<Users>>{
+            override fun onResponse(call: Call<List<Users>>, response: Response<List<Users>>) {
+                val usersList: List<Users> = response.body()!!
+                for(user in usersList){
+                    Log.d(TAG, "onResponse: name is ${user.name} and the email is ${user.email} ")
+                }
+            }
+
+            override fun onFailure(call: Call<List<Users>>, t: Throwable) {
+                Log.d(TAG, "the error is ${t}")
+            }
+
+        })
     }
 }
---
+++ app/src/main/java/com/example/future_android/Users.kt
@@ -0,0 +1,8 @@
+package com.example.future_android
+
+/**
+ * Created by sudharsan at 3/21/2023
+ */
+data class Users(var name:String, var email:String){
+
+}
